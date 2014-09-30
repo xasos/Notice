1ce2248dfea258cc9eaa2d69c694b0dab5d81f9c
Niraj Pant <nirajpant7@gmail.com>
2014-09-29 21:22:56 -0500

    A LOT of styling changes

--- www/dashboard/dashboard-controller.js
+++ www/dashboard/dashboard-controller.js
@@ -1,6 +1,11 @@
 'use strict';
 
-angular.module('MyApp.controllers').controller('DashboardCtrl', 
-  function() {
-
-  });
+angular.module('MyApp.controllers')
+.controller('DashboardCtrl', function($firebase, $scope, Auth) {
+  	var noticeRef = new Firebase('https://noticeapp.firebaseio.com/notifications');
+  	var textMessages = $firebase(noticeRef);
+  	var email = Auth.currentUser.email;
+  	$scope.postNotification = function(message) {
+  		textMessages.$add({message: message, createdBy: email, tag: tag});
+  	};
+});
