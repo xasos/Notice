da31604e32254c7f6b297ea878909c95219c0331
Niraj Pant <nirajpant7@gmail.com>
2014-10-02 13:09:54 -0500

    Update dashboard-controller.js

--- www/dashboard/dashboard-controller.js
+++ www/dashboard/dashboard-controller.js
@@ -4,8 +4,11 @@ angular.module('MyApp.controllers')
 .controller('DashboardCtrl', function($firebase, $scope, Auth) {
   	var noticeRef = new Firebase('https://noticeapp.firebaseio.com/notifications');
   	var textMessages = $firebase(noticeRef);
-  	var email = Auth.currentUser.email;
+  	$scope.email = Auth.currentUser.email;
+  	$scope.message = "";
   	$scope.postNotification = function(message) {
-  		textMessages.$add({message: message, createdBy: email, tag: tag});
+  		// textMessages.$add({message: message, createdBy: email, tag: tag});
+  		textMessages.$add({message: message, createdBy: email});
+  		message = "";
   	};
 });
