01eb2c37d0c642f68510e45df1b458a366cb42ab
Niraj Pant <nirajpant7@gmail.com>
2014-10-02 21:24:13 -0500

    view/firebase changes

--- www/dashboard/dashboard-controller.js
+++ www/dashboard/dashboard-controller.js
@@ -3,12 +3,16 @@
 angular.module('MyApp.controllers')
 .controller('DashboardCtrl', function($firebase, $scope, Auth) {
   	var noticeRef = new Firebase('https://noticeapp.firebaseio.com/notifications');
-  	var textMessages = $firebase(noticeRef);
+  	$scope.textMessages = $firebase(noticeRef);
+  	
   	$scope.email = Auth.currentUser.email;
   	$scope.message = "";
+
   	$scope.postNotification = function(message) {
-  		// textMessages.$add({message: message, createdBy: email, tag: tag});
-  		textMessages.$add({message: message, createdBy: $scope.email});
+  		// textMessages.$add({tag: tag});
+  		$scope.textMessages.$add({message: message, createdBy: $scope.email, dateCreated: Date.now()});
   		$scope.message = "";
-  	};
+  	};  	
 });
+
+//change textmessages to notifs
