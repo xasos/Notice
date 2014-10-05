c0c7494086e3265c8bdd5bdd4c90eabd18effd14
Niraj Pant <nirajpant7@gmail.com>
2014-10-05 18:49:28 -0500

    refactoring

--- www/dashboard/dashboard-controller.js
+++ www/dashboard/dashboard-controller.js
@@ -1,26 +1,36 @@
 'use strict';
 
 angular.module('MyApp.controllers')
-//.constant('BASE_URL', 'http://www.gravatar.com/avatar/')
+// .value('BASE_URL', 'http://www.gravatar.com/avatar/')
 .controller('DashboardCtrl', function($firebase, $scope, Auth, md5) {
   	var noticeRef = new Firebase('https://noticeapp.firebaseio.com/notifications');
-  	$scope.textMessages = $firebase(noticeRef);  	
+  	$scope.notifications = $firebase(noticeRef);  	
   	$scope.email = Auth.currentUser.email;
   	$scope.message = "";
-
-  	// this needs to be done for the person that created the url (push to firebase?)
-  	$scope.hashedEmail = md5.createHash($scope.email);
-  // 	$scope.gravatarURL = BASE_URL + $scope.hashedEmail;
-  // 	console.log($scope.gravatarURL);
+  	
+  	$scope.gravatarURL = 'http://www.gravatar.com/avatar/' + md5.createHash($scope.email); 
 
   	$scope.postNotification = function(message) {
-  		// textMessages.$add({tag: tag});
-  		$scope.textMessages.$add({message: message, createdBy: $scope.email, dateCreated: Date.now()});
-  		$scope.message = null;
-  	};  	 	  	
+  		// notifications.$add({tag: tag});
+  		  		// if ($scope.message.length == 0) {
+  		// 	$ionicLoading.show({
+    //     		template: 'Please try again!'
+    //   		});
+  		// }
+  		// else {
+  		// 	$scope.notifications.$add({message: message, createdBy: $scope.email, md5: $scope.hashedEmail, dateCreated: Date.now()});
+  		// 	$scope.message = null;
+  		// }
 
+  		$scope.notifications.$add({message: message, createdBy: $scope.email, gravatarURL: $scope.gravatarURL, dateCreated: Date.now()});
+  		$scope.message = null;
+  	};
+
+  	$scope.getGravatar = function(md5) {
+  		return 'http://www.gravatar.com/avatar/' + notifications.md5;
+  	};
   	//$scope.getGravatar = function($id) {
-  	//	Return BASE_URL + md5.createHash(textMessages.$id.createdBy);
+  	//	Return BASE_URL + md5.createHash(notifications.$id.createdBy);
   	//};
 });
 
