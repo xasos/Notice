a21b0018e96774250707cdca62f82af08da9edd2
Niraj Pant <nirajpant7@gmail.com>
2014-10-17 10:50:57 -0500

    Update dashboard-controller.js

--- www/dashboard/dashboard-controller.js
+++ www/dashboard/dashboard-controller.js
@@ -28,8 +28,6 @@ angular.module('MyApp.controllers')
 
   		$scope.notifications.$add({message: message, createdBy: $scope.email, gravatarURL: $scope.gravatarURL, dateCreated: Date.now(), tag: $scope.clubs[Math.floor(Math.random()*5)]});
   		$scope.message = null;
-        console.log($scope.activityName);
-        console.log($scope.clubs[Math.floor(Math.random()*5)]);
   	};
 
   	$scope.getGravatar = function(md5) {
