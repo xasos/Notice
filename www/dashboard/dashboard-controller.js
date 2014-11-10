de0be64e1e5b1e9d60e5287196ef0ff1c09761b0
Niraj Pant <nirajpant7@gmail.com>
2014-11-10 10:28:17 -0600

    Update dashboard-controller.js

--- www/dashboard/dashboard-controller.js
+++ www/dashboard/dashboard-controller.js
@@ -15,17 +15,17 @@ angular.module('MyApp.controllers')
     { id: 3, name: 'Horticulture Club', color: '#ef4e3a' },
     { id: 4, name: 'Science Olympiad', color: '#8a6de9'}];
 
-  	$scope.gravatarURL = 'http://www.gravatar.com/avatar/' + md5.createHash($scope.email); 
+    $scope.gravatarURL = 'http://www.gravatar.com/avatar/' + md5.createHash($scope.email); 
 
-  	$scope.postNotification = function(message, tag, color) {
-  	    console.log($scope.message);
-  		$scope.notifications.$add({message: message, createdBy: $scope.email, gravatarURL: $scope.gravatarURL, dateCreated: Date.now(), tag: tag, color: color});
-  		$scope.message = null;
-  	};
+    $scope.postNotification = function(message, tag, color) {
+        console.log($scope.message);
+  	$scope.notifications.$add({message: message, createdBy: $scope.email, gravatarURL: $scope.gravatarURL, dateCreated: Date.now(), tag: tag, color: color});
+  	$scope.message = null;
+    };
 
-  	$scope.getGravatar = function(md5) {
-  		return 'http://www.gravatar.com/avatar/' + notifications.md5;
-  	};
+    $scope.getGravatar = function(md5) {
+        return 'http://www.gravatar.com/avatar/' + notifications.md5;
+    };
 })
 .filter('reverse', function() {
       function toArray(list) {
