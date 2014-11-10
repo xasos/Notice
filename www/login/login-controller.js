4415dd96ccbeb83b4f7ef61dcc81141a18d838fd
Niraj Pant <nirajpant7@gmail.com>
2014-11-10 10:38:37 -0600

    Update login-controller.js

--- www/login/login-controller.js
+++ www/login/login-controller.js
@@ -16,6 +16,7 @@ angular.module('MyApp.controllers')
       });
  
       Auth.login($scope.user.email, $scope.user.password)
+          .then(console.log(Auth.getCurrentUser()))
           .then(User.loadCurrentUser)
           .then(redirectBasedOnStatus)
           .catch(handleError);
