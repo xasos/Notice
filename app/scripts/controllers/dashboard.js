e4be86de99c8d02a3604a566f665bd9b9b3acfdb
Niraj Pant <nirajpant7@gmail.com>
2014-11-13 10:23:20 -0600

    Update dashboard.js

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -8,10 +8,47 @@
  * Controller of the devFlowApp
  */
 angular.module('devFlowApp')
-  .controller('DashboardCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('DashboardCtrl', function ($scope, $firebase) {
+    var noticeRef = new Firebase('https://noticeapp.firebaseio.com/notifications');
+    $scope.notifications = $firebase(noticeRef);  	
+    console.log($scope.notifications);
+    $scope.email = '';
+    $scope.message = '';
+    $scope.activityName = '';
+    
+    $scope.clubs = [
+    { id: 1, name: 'Math Team', color: '#43cee6' },
+    { id: 2, name: 'Basketball', color: '#4a87ee' },
+    { id: 3, name: 'Horticulture Club', color: '#ef4e3a' },
+    { id: 4, name: 'Science Olympiad', color: '#8a6de9'}];
+
+    $scope.gravatarURL = 'http://www.gravatar.com/avatar/' + md5.createHash($scope.email); 
+
+    $scope.postNotification = function(message, tag, color) {
+        $scope.notifications.$add({message: message, createdBy: $scope.email, gravatarURL: $scope.gravatarURL, dateCreated: Date.now(), tag: tag, color: color});
+  	    $scope.message = null;
+    };
+
+    $scope.getGravatar = function(md5) {
+        return 'http://www.gravatar.com/avatar/' + notifications.md5;
+    };
+})
+.filter('reverse', function() {
+      function toArray(list) {
+         var k, out = [];
+         if( list ) {
+            if( angular.isArray(list) ) {
+               out = list;
+            }
+            else if( typeof(list) === 'object' ) {
+               for (k in list) {
+                  if (list.hasOwnProperty(k)) { out.push(list[k]); }
+               }
+            }
+         }
+         return out;
+      }
+      return function(items) {
+         return toArray(items).slice().reverse();
+      };
+});
