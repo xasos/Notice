b463dadca66062a430c19278015b7da229c6fa6b
Niraj Pant <nirajpant7@gmail.com>
2014-11-19 13:35:55 -0600

    Update user-service.js

--- www/user/user-service.js
+++ www/user/user-service.js
@@ -1,11 +1,10 @@
 'use strict';
 
 angular.module('MyApp.services').service('User',
-  function($q, $firebase, FIREBASE_ROOT, Auth, $rootScope) {
+  function($q, $firebase, FIREBASE_ROOT, Auth) {
     var usersRef = new Firebase(FIREBASE_ROOT + '/users');
     var currentUser = null;
-    var subscribed = "1234";
-    
+
     this.loadCurrentUser = function() {
       var defer = $q.defer();
       var currentUserRef = usersRef.child(Auth.currentUser.uid);
@@ -18,34 +17,17 @@ angular.module('MyApp.services').service('User',
 
     this.create = function(id, email) {
       var users = $firebase(usersRef);
-      return users.$child(id).$push({ email: email });
+
+      return users.$child(id).$set({ email: email });
     };
-    
-    // this.addName = function(firstName, lastName) {
-    //   var users = $firebase(usersRef);
-    //   return users.$child(id).$add({ first: firstName, last:lastName });
-    // };
-    
+
     this.recordPasswordChange = function() {
       var now = Math.floor(Date.now() / 1000);
+      
       return currentUser.$update({ passwordLastChangedAt: now });
     };
 
     this.hasChangedPassword = function() {
       return angular.isDefined(currentUser.passwordLastChangedAt);
-    };
-    
-    this.manageSubscriptions = function(subs) {
-      var users = $firebase(usersRef);
-      
-      for (var key in users) {
-            var obj = users[key];
-            if (key.toLowerCase() === Auth.currentUser.uid) {
-               subscribed = subs;
-               console.log(subscribed);
-            }
-      }
-      
-      return users.$child(Auth.currentUser.uid).$set({ subscribed: subs });
     };
   });
