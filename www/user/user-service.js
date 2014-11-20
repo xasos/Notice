f3744d07e43c50a220dcfce4540b0451a3ba055d
Niraj Pant <nirajpant7@gmail.com>
2014-11-20 10:22:49 -0600

    Update user-service.js

--- www/user/user-service.js
+++ www/user/user-service.js
@@ -41,6 +41,9 @@ angular.module('MyApp.services').service('User',
     this.getSubscriptions = function() {		
       var users = $firebase(usersRef);		
       var uid = Auth.currentUser.uid;
+      console.log(uid);
+      console.log(users);
+      console.log(users.uid.subscriptions);
       return users.uid.subscriptions;
     };
     
